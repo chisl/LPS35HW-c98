7bf8554da73d0a8a6a4bfea940fc31fb525f16bf
chisl <info@chisl.io>
2018-01-03 00:53:23 +0100

    Initial commit

---
+++ LPS35HW.h
@@ -0,0 +1,15 @@
+/*
+ * name:        LPS35HW
+ * description: 260-1260 hPa absolute digital output barometer with water resistant package
+ * manuf:       STMicroelectronics
+ * version:     0.1
+ * url:         http://www.st.com/resource/en/datasheet/lps35hw.pdf
+ * date:        2018-01-04
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
